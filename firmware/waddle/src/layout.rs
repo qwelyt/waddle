f36e4d530a19bb3b3b72aa4f4cab44473f73549d
qwelyt <qwelyt@users.noreply.github.com>
2023-04-13 22:06:26 +0000

    Working keyboard
    Now everything works! Putting the layout in PROGMEM really saved this.
    And the nice thing is that we are only pulling the byte that we actually
    care about out of progmem, the rest of the layout stays put!
    
    Now it's time to start setting a layout I suppose.

--- firmware/waddle/src/layout.rs
+++ firmware/waddle/src/layout.rs
@@ -1,3 +1,6 @@
+use avr_progmem::progmem;
+use avr_progmem::wrapper::ProgMem;
+
 use k::norde::se;
 use Key::{Dead, KeyCode, LayerCh};
 
@@ -7,6 +10,7 @@ use crate::layout::Key::{Function, LayerMo};
 use crate::position::position::Position;
 use crate::state::State;
 
+#[derive(Copy, Clone)]
 pub enum Key {
     KeyCode(u8),
     Function(fn(&mut State)),
@@ -19,42 +23,41 @@ pub const ROWS: usize = 4;
 pub const COLS: usize = 12;
 pub const BUTTONS: usize = ROWS * COLS;
 pub const NUM_CHUNKS: usize = BUTTONS / 6;
-pub const LAYERS: usize = 1;
+pub const LAYERS: usize = 4;
 pub const LEDS: usize = 2;
-// pub const MATRIX: [[[Key; COLS]; ROWS]; LAYERS] = [
-//     [
-//         [KeyCode(k::A), KeyCode(k::B)],
-//         [KeyCode(k::C), LayerMo(1)],
-//     ],
-//     [
-//         [KeyCode(k::R_SHFT), KeyCode(k::E)],
-//         [KeyCode(k::F), Dead],
-//     ],
-// ];
-pub const MATRIX: [[[Key; COLS]; ROWS]; LAYERS] = [
-    [
-        [KeyCode(k::TAB), KeyCode(k::Q), KeyCode(k::W), KeyCode(k::E), KeyCode(k::R), KeyCode(k::T), KeyCode(k::Y), KeyCode(k::U), KeyCode(k::I), KeyCode(k::O), KeyCode(k::P), KeyCode(se::Å), ],
-        [KeyCode(k::ESC), KeyCode(k::A), KeyCode(k::S), KeyCode(k::D), KeyCode(k::F), KeyCode(k::G), KeyCode(k::H), KeyCode(k::J), KeyCode(k::K), KeyCode(k::L), KeyCode(se::Ö), KeyCode(se::Ä), ],
-        [KeyCode(k::L_SHFT), KeyCode(k::Z), KeyCode(k::X), KeyCode(k::C), KeyCode(k::V), KeyCode(k::B), KeyCode(k::N), KeyCode(k::M), KeyCode(k::COMMA), KeyCode(k::DOT), KeyCode(se::DASH), KeyCode(k::R_SHFT), ],
-        [KeyCode(k::L_CTRL), KeyCode(k::L_SUPR), KeyCode(k::BS_N_PIPE), KeyCode(k::L_ALT), LayerMo(1), KeyCode(k::SPACE), KeyCode(k::RETURN), LayerMo(2), KeyCode(k::R_ALT), KeyCode(k::MENU), KeyCode(k::R_SUPR), KeyCode(k::R_CTRL), ],
-    ],
-    // [
-    //     [KeyCode(k::TAB), KeyCode(k::Q), KeyCode(k::W), KeyCode(k::E), KeyCode(k::R), KeyCode(k::T), KeyCode(k::Y), KeyCode(k::U), KeyCode(k::I), KeyCode(k::O), KeyCode(k::P), KeyCode(se::Å), ],
-    //     [KeyCode(k::ESC), KeyCode(k::A), KeyCode(k::S), KeyCode(k::D), KeyCode(k::F), KeyCode(k::G), KeyCode(k::H), KeyCode(k::J), KeyCode(k::K), KeyCode(k::L), KeyCode(se::Ö), KeyCode(se::Ä), ],
-    //     [KeyCode(k::L_SHFT), KeyCode(k::Z), KeyCode(k::X), KeyCode(k::C), KeyCode(k::V), KeyCode(k::B), KeyCode(k::N), KeyCode(k::M), KeyCode(k::COMMA), KeyCode(k::DOT), KeyCode(se::DASH), KeyCode(k::R_SHFT), ],
-    //     // [KeyCode(k::L_CTRL), KeyCode(k::L_SUPR), KeyCode(k::BS_N_PIPE), KeyCode(k::L_ALT), Dead, KeyCode(k::SPACE), KeyCode(k::RETURN), Dead, KeyCode(k::R_ALT), KeyCode(k::MENU), KeyCode(k::R_SUPR), KeyCode(k::R_CTRL), ],
-    // ],
-    // [
-    //     [KeyCode(k::TAB), KeyCode(k::Q), KeyCode(k::W), KeyCode(k::E), KeyCode(k::R), KeyCode(k::T), KeyCode(k::Y), KeyCode(k::U), KeyCode(k::I), KeyCode(k::O), KeyCode(k::P), KeyCode(se::Å), ],
-    //     [KeyCode(k::ESC), KeyCode(k::A), KeyCode(k::S), KeyCode(k::D), KeyCode(k::F), KeyCode(k::G), KeyCode(k::H), KeyCode(k::J), KeyCode(k::K), KeyCode(k::L), KeyCode(se::Ö), KeyCode(se::Ä), ],
-    //     [KeyCode(k::L_SHFT), KeyCode(k::Z), KeyCode(k::X), KeyCode(k::C), KeyCode(k::V), KeyCode(k::B), KeyCode(k::N), KeyCode(k::M), KeyCode(k::COMMA), KeyCode(k::DOT), KeyCode(se::DASH), KeyCode(k::R_SHFT), ],
-    //     // [KeyCode(k::L_CTRL), KeyCode(k::L_SUPR), KeyCode(k::BS_N_PIPE), KeyCode(k::L_ALT), Dead, KeyCode(k::SPACE), KeyCode(k::RETURN), Dead, KeyCode(k::R_ALT), KeyCode(k::MENU), KeyCode(k::R_SUPR), KeyCode(k::R_CTRL), ],
-    // ],
-];
+progmem! {
+    pub static progmem MATRIX: [[[Key; COLS]; ROWS]; LAYERS] = [
+        [
+            [KeyCode(k::TAB), KeyCode(k::Q), KeyCode(k::W), KeyCode(k::E), KeyCode(k::R), KeyCode(k::T), KeyCode(k::Y), KeyCode(k::U), KeyCode(k::I), KeyCode(k::O), KeyCode(k::P), KeyCode(se::Å), ],
+            [KeyCode(k::ESC), KeyCode(k::A), KeyCode(k::S), KeyCode(k::D), KeyCode(k::F), KeyCode(k::G), KeyCode(k::H), KeyCode(k::J), KeyCode(k::K), KeyCode(k::L), KeyCode(se::Ö), KeyCode(se::Ä), ],
+            [KeyCode(k::L_SHFT), KeyCode(k::Z), KeyCode(k::X), KeyCode(k::C), KeyCode(k::V), KeyCode(k::B), KeyCode(k::N), KeyCode(k::M), KeyCode(k::COMMA), KeyCode(k::DOT), KeyCode(se::DASH), KeyCode(k::R_SHFT), ],
+            [KeyCode(k::L_CTRL), KeyCode(k::L_SUPR), KeyCode(k::BS_N_PIPE), KeyCode(k::L_ALT), LayerMo(1), KeyCode(k::SPACE), KeyCode(k::RETURN), LayerMo(2), KeyCode(k::R_ALT), KeyCode(k::MENU), KeyCode(k::R_SUPR), KeyCode(k::R_CTRL), ],
+        ],
+        [
+            [KeyCode(k::K1), KeyCode(k::K2), KeyCode(k::K3), KeyCode(k::K4), KeyCode(k::K5), KeyCode(k::K6), KeyCode(k::K7), KeyCode(k::K8), KeyCode(k::K9), KeyCode(k::K0), KeyCode(k::OBRAKET), KeyCode(k::CBRAKET), ],
+            [KeyCode(k::ESC), KeyCode(k::A), KeyCode(k::S), KeyCode(k::D), KeyCode(k::F), KeyCode(k::G), KeyCode(k::H), KeyCode(k::J), KeyCode(k::K), KeyCode(k::L), KeyCode(se::Ö), KeyCode(se::Ä), ],
+            [KeyCode(k::L_SHFT), KeyCode(k::Z), KeyCode(k::X), KeyCode(k::C), KeyCode(k::V), KeyCode(k::B), KeyCode(k::N), KeyCode(k::M), KeyCode(k::COMMA), KeyCode(k::DOT), KeyCode(se::DASH), KeyCode(k::BACKSPACE), ],
+            [KeyCode(k::L_CTRL), KeyCode(k::L_SUPR), KeyCode(k::BS_N_PIPE), KeyCode(k::L_ALT), Dead, KeyCode(k::SPACE), KeyCode(k::RETURN), Dead, KeyCode(k::R_ALT), KeyCode(k::MENU), KeyCode(k::R_SUPR), KeyCode(k::R_CTRL), ],
+        ],
+        [
+            [KeyCode(k::TAB), KeyCode(k::Q), KeyCode(k::W), KeyCode(k::E), KeyCode(k::R), KeyCode(k::T), KeyCode(k::Y), KeyCode(k::U), KeyCode(k::I), KeyCode(k::O), KeyCode(k::P), KeyCode(se::Å), ],
+            [KeyCode(k::ESC), KeyCode(k::A), KeyCode(k::S), KeyCode(k::D), KeyCode(k::F), KeyCode(k::G), KeyCode(k::H), KeyCode(k::J), KeyCode(k::K), KeyCode(k::L), KeyCode(se::Ö), KeyCode(se::Ä), ],
+            [KeyCode(k::L_SHFT), KeyCode(k::Z), KeyCode(k::X), KeyCode(k::C), KeyCode(k::V), KeyCode(k::B), KeyCode(k::N), KeyCode(k::M), KeyCode(k::COMMA), KeyCode(k::DOT), KeyCode(se::DASH), KeyCode(k::R_SHFT), ],
+            [KeyCode(k::L_CTRL), KeyCode(k::L_SUPR), KeyCode(k::BS_N_PIPE), KeyCode(k::L_ALT), Dead, KeyCode(k::SPACE), KeyCode(k::RETURN), Dead, KeyCode(k::R_ALT), KeyCode(k::MENU), KeyCode(k::R_SUPR), KeyCode(k::R_CTRL), ],
+        ],
+        [
+            [KeyCode(k::TAB), KeyCode(k::Q), KeyCode(k::W), KeyCode(k::E), KeyCode(k::R), KeyCode(k::T), KeyCode(k::Y), KeyCode(k::U), KeyCode(k::I), KeyCode(k::O), KeyCode(k::P), KeyCode(se::Å), ],
+            [KeyCode(k::ESC), KeyCode(k::A), KeyCode(k::S), KeyCode(k::D), KeyCode(k::F), KeyCode(k::G), KeyCode(k::H), KeyCode(k::J), KeyCode(k::K), KeyCode(k::L), KeyCode(se::Ö), KeyCode(se::Ä), ],
+            [KeyCode(k::L_SHFT), KeyCode(k::Z), KeyCode(k::X), KeyCode(k::C), KeyCode(k::V), KeyCode(k::B), KeyCode(k::N), KeyCode(k::M), KeyCode(k::COMMA), KeyCode(k::DOT), KeyCode(se::DASH), KeyCode(k::R_SHFT), ],
+            [KeyCode(k::L_CTRL), KeyCode(k::L_SUPR), KeyCode(k::BS_N_PIPE), KeyCode(k::L_ALT), Dead, KeyCode(k::SPACE), KeyCode(k::RETURN), Dead, KeyCode(k::R_ALT), KeyCode(k::MENU), KeyCode(k::R_SUPR), KeyCode(k::R_CTRL), ],
+        ],
+    ];
+}
+
 pub static LAYOUT: Layout = Layout { matrix: MATRIX };
 
 pub struct Layout {
-    matrix: [[[Key; COLS]; ROWS]; LAYERS],
+    matrix: ProgMem<[[[Key; COLS]; ROWS]; LAYERS]>,
 }
 
 
@@ -63,7 +66,9 @@ impl Layout {
         Self { matrix: MATRIX }
     }
     pub fn get_layer_mod(&self, position: &Position) -> u8 {
-        match self.matrix[0][position.row() as usize][position.col() as usize] {
+        match self.matrix.at(0)
+            .at(position.row() as usize)
+            .load_at(position.col() as usize) {
             KeyCode(_) => { 0 }
             Function(_) => { 0 }
             LayerMo(l) => { l }
@@ -73,11 +78,10 @@ impl Layout {
     }
 
     pub fn get_keycode(&self, layer: u8, position: &Position) -> Option<u8> {
-        match self.matrix.get(layer as usize)
-            .and_then(|layer| layer.get(position.row() as usize))
-            .and_then(|row| row.get(position.col() as usize))
-            .unwrap_or(&Dead) {
-            KeyCode(kc) => { Some(*kc) }
+        match self.matrix.at(layer as usize)
+            .at(position.row() as usize)
+            .load_at(position.col() as usize) {
+            KeyCode(kc) => { Some(kc) }
             Function(_) => { None }
             LayerMo(_) => { None }
             LayerCh(_) => { None }

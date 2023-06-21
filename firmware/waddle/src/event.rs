413c5d155d833393169e14b43546c50c31e919c8
qwelyt <qwelyt@users.noreply.github.com>
2023-06-21 19:48:33 +0000

    Add odd comment

--- firmware/waddle/src/event.rs
+++ firmware/waddle/src/event.rs
@@ -1,5 +1,8 @@
 #[derive(Eq, PartialEq, Copy, Clone)]
 pub enum Event {
     KeyCode(u8),
+    // Adding another enum here breaks *everything*. Does not matter if it's used or not, this
+    // Breaks the USB setup. I can add enums in `Key` without problem, but here it breaks.
+    // Function
 }
 
